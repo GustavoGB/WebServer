408013372c0ea97384c7ac8ce4567fb6a3bf0f55
GustavoGB <gustavo.gobetti98@gmail.com>
2020-06-11 16:12:35 -0300

    corrigindo script de deploy

--- install_web_dep.sh
+++ install_web_dep.sh
@@ -1,15 +1,11 @@
 sudo apt-get update -y  && sudo apt-get upgrade -y 
 sudo apt install python3-pip -y 
-sudo apt install docker.io
-sudo apt install virtualenv
+sudo apt install docker.io -y 
+sudo apt install virtualenv -y 
 virtualenv env
 source ./env/bin/activate
-cd /home/ubuntu/
-git clone https://github.com/GustavoGB/cloud_project_serveless.git
-cd /cloud_project_serveless/
 sudo pip3 install fastapi
 sudo pip3 install uvicorn 
-cd cloud_project_serveless/
 uvicorn webserver:app --reload
 
 
