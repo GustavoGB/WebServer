7e7ef4f92ae55741956901203bfa4060ff1cb917
GustavoGB <gustavo.gobetti98@gmail.com>
2020-06-11 19:29:12 -0300

    host

--- install_web_dep.sh
+++ install_web_dep.sh
@@ -1,12 +1,12 @@
 sudo apt-get update -y  && sudo apt-get upgrade -y 
 sudo apt install python3-pip -y 
 sudo apt install docker.io -y 
-sudo apt install virtualenv -y 
-virtualenv env
-source ./env/bin/activate
+#sudo apt install virtualenv -y 
+#virtualenv env
+#source ./env/bin/activate
 sudo pip3 install fastapi
 sudo pip3 install uvicorn 
-uvicorn webserver:app --reload
+uvicorn --host 0.0.0.0 webserver:app --reload
 
 
 
